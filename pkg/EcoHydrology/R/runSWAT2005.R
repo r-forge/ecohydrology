eb8e5731cc5d3c1d71b495cb8f5cb3548f2e99f5
zme2 (Dr ) <zeaston@vt.edu>
2011-04-05 18:10:37 +0000

    added system environment control
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydrology/R/runSWAT2005.R
+++ pkg/EcoHydrology/R/runSWAT2005.R
@@ -1,6 +1,8 @@
 runSWAT2005 <-
 function(){
 Sys.setenv(GFORTRAN_STDIN_UNIT=-1)
+Sys.setenv(GFORTRAN_STDOUT_UNIT=-1)
+Sys.setenv(GFORTRAN_STDERR_UNIT=-1)
 library.dynam("rswat","EcoHydrology")
 .Fortran("swat2005",1)
 library.dynam.unload("rswat",grep("EcoHydrology",searchpaths(),value=T))
