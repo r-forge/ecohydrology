d56bb57989ed08967a985b73656dd6d65416fe4b
drfuka (Mr. ) <drf28@cornell.edu>
2011-04-05 01:38:29 +0000

    Adding first alpha of swat calibration utilities
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydrology/R/runSWAT2005.R
+++ pkg/EcoHydrology/R/runSWAT2005.R
@@ -3,5 +3,6 @@ function(){
 Sys.setenv(GFORTRAN_STDIN_UNIT=-1)
 library.dynam("rswat","EcoHydrology")
 .Fortran("swat2005",1)
+library.dynam.unload("rswat",grep("EcoHydrology",searchpaths(),value=T))
 }
 
