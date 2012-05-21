87b4c3b2ac204fd0c63d05193f2c8a31fba11f43
drfuka (Mr. ) <drf28@cornell.edu>
2012-05-21 20:36:57 +0000

    Adding in SWAT2009
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@98 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydRology/src/install.libs.R
+++ pkg/EcoHydRology/src/install.libs.R
@@ -1,5 +1,10 @@
-files <- Sys.glob("*.exe")
-libarch <- if (nzchar(R_ARCH)) paste('libs', R_ARCH, sep='') else 'libs'
+## Modified from Rserve
+libarch <- if (nzchar(R_ARCH)) paste("libs", R_ARCH, sep='') else "libs"
 dest <- file.path(R_PACKAGE_DIR, libarch)
-dir.create(dest, recursive = TRUE, showWarnings = FALSE)
-file.copy(files, dest, overwrite = TRUE)
+
+files <- if (WINDOWS) "rswat2005.exe" else "rswat2005"
+if (length(files)) {
+  dir.create(dest, recursive = TRUE, showWarnings = FALSE)
+  file.copy(files, dest, overwrite = TRUE)
+}
+
