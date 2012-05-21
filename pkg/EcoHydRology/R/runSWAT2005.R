8d4ef50d5370d2dfda9899697e417654489b6c85
drfuka (Mr. ) <drf28@cornell.edu>
2012-05-21 20:39:41 +0000

    updating version number
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@99 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydRology/R/runSWAT2005.R
+++ pkg/EcoHydRology/R/runSWAT2005.R
@@ -3,8 +3,11 @@ function(){
 Sys.setenv(GFORTRAN_STDIN_UNIT=-1)
 Sys.setenv(GFORTRAN_STDOUT_UNIT=-1)
 Sys.setenv(GFORTRAN_STDERR_UNIT=-1)
-library.dynam("EcoHydRology",package="EcoHydRology")
-.Fortran("swat2005",PACKAGE="EcoHydRology",1)
-library.dynam.unload("EcoHydRology",grep("EcoHydRology",searchpaths(),value=T))
+libarch= if (nzchar(version$arch)) paste('libs', version$arch, sep='/') else 'libs'
+system(paste(.Library,"/EcoHydRology/",libarch,"/swat2005.exe",sep=""))
+
+#library.dynam("EcoHydRology",package="EcoHydRology")
+#.Fortran("swat2005",PACKAGE="EcoHydRology",1)
+#library.dynam.unload("EcoHydRology",grep("EcoHydRology",searchpaths(),value=T))
 }
 
