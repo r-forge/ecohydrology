a6fca7f90b74c84da40219d8959af946b8cc1634
drfuka (Mr. ) <drf28@cornell.edu>
2012-05-22 02:53:32 +0000

    Update to runswat2005 script
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@101 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydRology/R/runSWAT2005.R
+++ pkg/EcoHydRology/R/runSWAT2005.R
@@ -4,7 +4,8 @@ Sys.setenv(GFORTRAN_STDIN_UNIT=-1)
 Sys.setenv(GFORTRAN_STDOUT_UNIT=-1)
 Sys.setenv(GFORTRAN_STDERR_UNIT=-1)
 libarch= if (nzchar(version$arch)) paste('libs', version$arch, sep='/') else 'libs'
-system(paste(.Library,"/EcoHydRology/",libarch,"/swat2005.exe",sep=""))
+swatbin<- "rswat2005.exe"
+system(paste(.Library,"/EcoHydRology/",libarch,"/",swatbin,sep=""))
 
 #library.dynam("EcoHydRology",package="EcoHydRology")
 #.Fortran("swat2005",PACKAGE="EcoHydRology",1)
--- pkg/EcoHydRology/src/install.libs.R
+++ pkg/EcoHydRology/src/install.libs.R
@@ -2,7 +2,8 @@
 libarch <- if (nzchar(R_ARCH)) paste("libs", R_ARCH, sep='') else "libs"
 dest <- file.path(R_PACKAGE_DIR, libarch)
 
-files <- if (WINDOWS) "rswat2005.exe" else "rswat2005"
+files <- "rswat2005.exe"
+
 if (length(files)) {
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
   file.copy(files, dest, overwrite = TRUE)
