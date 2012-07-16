6f897d225a3d1538c5252f7e311acaa944d719a4
drfuka (Mr. ) <drf28@cornell.edu>
2012-07-16 07:23:32 +0000

    Cleanup
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@119 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydRology/R/swat_objective_function.R
+++ pkg/EcoHydRology/R/swat_objective_function.R
@@ -1,8 +1,13 @@
-swat_objective_function<-function(x,calib_range,calib_params){
+swat_objective_function<-function(x,calib_range,calib_params,flowgage){
+
 calib_params$current<-x
 system("rm output.*")
 alter_files(calib_params)
-runSWAT2005()
+
+libarch = if (nzchar(version$arch)) paste("libs", version$arch, sep = "/") else "libs"
+swatbin <- "rswat2005.exe"
+system(paste(path.package("SWATmodel"), libarch, swatbin, sep = "/"))
+
 start_year=read.fortran(textConnection(readLines("file.cio")[9]),"f20")
 test=readLines(file("output.rch"))
 rchcolname=sub(" ","",(substr(test[9],50,61)))
