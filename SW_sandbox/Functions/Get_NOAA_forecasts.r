e2fec270cd30656243b481e6bf1e4f472f90f751
jaa78 (Josephine Archibald) <jaa78@cornell.edu>
2013-07-26 21:01:56 +0000

    Corrected the NOAA forecast function to get predictions at a more consistent time of day, converting the UTM time-stamp to local time zone.  Also corrected the spelling error in the function name.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@183 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- SW_sandbox/Functions/Get_NOAA_forecasts.r
+++ SW_sandbox/Functions/Get_NOAA_forecasts.r
@@ -12,7 +12,7 @@
 ##	Fulton:		KFZY
 ##  Can help find stations: http://weather.uwyo.edu/models/mos/mos.html
 
-forecastsNOAA <- function(stn = c("KITH", "KSYR") , probCut = 30, numbDays = 3, PrintDataTables = TRUE) {
+forecastsNOAA <- function(stn = c("KITH", "KSYR") , probCut = 30, numbDays = 5, PrintDataTables = FALSE) {
 		
 	##  This allows us to convert from the numbers in the prediction table, to an actual depth of precip (mm)
 	##	Since we want the tool to be conservative, I am using the max precip depth for that code, 
@@ -27,16 +27,7 @@ forecastsNOAA <- function(stn = c("KITH", "KSYR") , probCut = 30, numbDays = 3, 
 	
 ##  Gather forecasts and recent data for each station: 	
 	for (i in 1:length(stn) ){
-		PrbPrecip <- vector(length = numbDays)
-		
-	#  We first need to get the most recent 6 hours of recorded data from this station: 
-		library(XML)
-		# temp <- readHTMLTable(paste("http://www.nws.noaa.gov/data/obhistory/", stn[i], ".html", sep=""), skip.rows=c(1,2,3,70,71,72,73,74,75,76,77,78), header=FALSE)[[4]]   ##  This one seems slower to update? 
-		Cur6hr <- readHTMLTable(paste("http://w1.weather.gov/data/obhistory/", stn[i], ".html", sep=""), skip.rows=c(1,2,3,70,71,72,73,74,75,76,77,78), header=FALSE, stringsAsFactors=FALSE)[[4]]
-		colnames(Cur6hr) <- c("day", "time", "wind_mph", "vis_mi", "weath", "Sky", "AirTempF", "DewPt_F", "Temp6hrMax", "Temp6hrMin", "RH", "WindChill_F", "HeatIndex_F", "Press_alt_in", "Press_SL_mb", "Prec_1hr_in", "Prec_3hr_in","Prec_6hr_in")
-		MaxTemp6 <- max(na.omit(as.numeric(c(Cur6hr[1:6,9],Cur6hr[1:6,7]))))  		# F (will be converted below)
-		MinTemp6 <- min(na.omit(as.numeric(c(Cur6hr[1:6,10],Cur6hr[1:6,7]))))	
-		precip6 <- suppressWarnings(sum(na.omit(as.numeric(Cur6hr$Prec_1hr_in[1:6])))) * 25.4		# mm
+		PredTime <- vector(length = numbDays)
 		
 	#  Read in forecasts from the short-term forecast (MOS and MAV), and format it (final table called ForecastTable):
 		forecasts <- read.fwf(paste("http://www.nws.noaa.gov/cgi-bin/mos/getmav.pl?sta=", stn[i], sep=""), fill=TRUE, skip=7, row.names=NULL, widths=c(5,rep(3,21)),colClasses="character")[1:20,]
@@ -47,6 +38,8 @@ forecastsNOAA <- function(stn = c("KITH", "KSYR") , probCut = 30, numbDays = 3, 
 		f2 <- f1[-1,]
 		colnames(f2) <- tis
 		ForecastTable <- suppressWarnings(data.frame(apply(f2,2,as.numeric)))    # Convert to numeric. Final data frame. 
+		TimeDif <- Sys.time() - as.POSIXlt(strptime(substr(StationInfo[2*i-1],40,47),format="%H%M", tz="UTC"))
+		PredTime[1] <- as.character(Sys.time() - TimeDif + 30*3600)    ##  Puts it into Eastern Standard Time insted of UTM
 		
 	#  If we want forecasts for more than 3 days, we need to get the extended forecast : MOS and MEX 
 		forecasts <- read.fwf(paste("http://www.nws.noaa.gov/cgi-bin/mos/getmex.pl?sta=", stn[i], sep=""), fill=TRUE, skip=6, row.names=NULL, widths=c(5,rep(4,15)),colClasses="character")[1:16,1:14]
@@ -58,34 +51,33 @@ forecastsNOAA <- function(stn = c("KITH", "KSYR") , probCut = 30, numbDays = 3, 
 		colnames(f2) <- tis
 		ForecastTableExtend <- suppressWarnings(data.frame(apply(f2,2,as.numeric)))    # Convert to numeric. Final data frame
 	
-	#  First day's forecast is patched together from 6 hours earlier, and the first 3 6-hr predictions from MAV
-		MxT[1,i] <- (max(ForecastTable$TMP[1:7],MaxTemp6)-32)*5/9 # Max temp predictions 
-		MnT[1,i] <- (min(ForecastTable$TMP[1:7],MinTemp6)-32)*5/9 # Min temp predictions 
-		Precip[1,i] <- sum(MAVcodes[2, match(ForecastTable$Q06[which(ForecastTable$P06[1:8] > probCut)], MAVcodes[1,] )],precip6)  
+	#  First day's forecast is from the first 9 prediction rows from MAV 
+	# (a reading at 0600 UTM will give a 24-hr prediction from 7 am - 7 am EST or 8 am - 8 am during daylight savings time.)
+		MxT[1,i] <- (max(ForecastTable$MxMn[1:9], na.rm=TRUE)-32)*5/9 # Max temp predictions 
+		MnT[1,i] <- (min(ForecastTable$MxMn[1:9], na.rm=TRUE)-32)*5/9 # Min temp predictions 
+		Precip[1,i] <- sum(MAVcodes[2, match(ForecastTable$Q06[which(ForecastTable$P06[1:9] > probCut)], MAVcodes[1,] )])  
 		Probs[1,i] <- max(ForecastTable$P06[1:8], na.rm=TRUE)
+		#  Find the times associated with the predictions:
+		PredTimeList <- GetPredictionTime(chartExt=ForecastTableExtend, stnHead = StationInfo[2*i])
 		
 	#  Now we will pull out Max and Min temps, and predicted precipitation for the rest of the prediction from the long-term forecast
 		for (j in 2:numbDays){
-			if (TRUE %in% is.na(ForecastTableExtend$MxMn[(2*j-3):(2*j-2)])){  # If 24 hr Max/Min Problem, probably won't use
+			if (TRUE %in% is.na(ForecastTableExtend$MxMn[(2*j-1):(2*j)])){  # If 24 hr Max/Min Problem, probably won't use
 				print("Using 12-hr TEMP instead of 24-hr MxMn")
-				MxT[j,i] <- (max(ForecastTableExtend$TMP[(2*j-3):(2*j-2)])-32)*5/9 # Not as accurate max/min result
-				MnT[j,i] <- (min(ForecastTableExtend$TMP[(2*j-3):(2*j-2)])-32)*5/9 # Not as accurate max/min result
+				MxT[j,i] <- (max(ForecastTableExtend$TMP[(2*j-1):(2*j)])-32)*5/9 # Not as accurate max/min result
+				MnT[j,i] <- (min(ForecastTableExtend$TMP[(2*j-1):(2*j)])-32)*5/9 # Not as accurate max/min result
 			} else{		# This is default
-			MxT[j,i] <- (max(ForecastTableExtend$MxMn[(2*j-3):(2*j-2)])-32)*5/9 # Max temp predictions 
-			MnT[j,i] <- (min(ForecastTableExtend$MxMn[(2*j-3):(2*j-2)])-32)*5/9 # Min temp predictions 
+			MxT[j,i] <- (max(ForecastTableExtend$MxMn[(2*j-1):(2*j)])-32)*5/9 # Max temp predictions 
+			MnT[j,i] <- (min(ForecastTableExtend$MxMn[(2*j-1):(2*j)])-32)*5/9 # Min temp predictions 
 			}
-			if (is.na(ForecastTableExtend$P24[j-1]) | is.na(ForecastTableExtend$Q24[j-1])){
-				print("Using Q12 instead of Q24")
-				Precip[j,i] <- sum(MAVcodes[2, match(ForecastTableExtend$Q12[which(ForecastTableExtend$P12[(2*j-3):(2*j-2)] > probCut)], MAVcodes[1,] )],0)
-				Probs[j,i] <- max( ForecastTableExtend$P12[(2*j-3):(2*j-2)] )
-			} else {
-				Precip[j,i] <- ifelse(ForecastTableExtend$P24[j-1] > probCut, MAVcodes[2, which(MAVcodes[1,] == ForecastTableExtend$Q24[j-1])], 0)  
-				Probs[j,i] <- ForecastTableExtend$P24[j-1]
-			}
-			
+			PredTime[j] <- as.character(PredTimeList[2*j])  # Prediction is for 24 hours up to this time.
+		#  If either 12-hour probability of precip is greater than our cutoff, we will have the greater of:
+			# 2 mm of rain or the sum of the 12-hour predicted rain amounts
+			Probs[j,i] <- max( ForecastTableExtend$P12[(2*j-1):(2*j)] )
+			Precip[j,i] <- ifelse(Probs[j,i] > probCut, max(MAVcodes[2,2],
+				sum(MAVcodes[2, match(ForecastTableExtend$Q12[(2*j-1:0)][which(ForecastTableExtend$P12[(2*j-1:0)] > probCut)], MAVcodes[1,] )],0) ) , 0 )
 		}
-	
-	
+
 	if (PrintDataTables == TRUE) {
 		DateTime <- strftime(Sys.time(), format='%Y%m%d%H%M')
 		Year <- 1900 + as.POSIXlt(as.Date(Sys.time()))$year
@@ -100,19 +92,25 @@ forecastsNOAA <- function(stn = c("KITH", "KSYR") , probCut = 30, numbDays = 3, 
 				print(ForecastTable)
 		if (i == length(stn)) 	sink()   # Stops printing to the output file at last station
 	}
+	
+	#  Print the predictions for each station:
+	if (b == "OwascoLake"){
+		print(StationInfo[i*2])
+		print(data.frame(PredTime = as.POSIXct(PredTime, origin = "1970-01-01"),ProbPrc = Probs[,i], P = Precip[,i], Tx = MxT[,i], Tn = MnT[,i]))
 	}
-	##  Just for checking that this is working initially: (can delete these lines soon)
-	cat("Daily FORECASTS:\n")
-	print(StationInfo)
-	cat("Max temp\n")
-	print(MxT)
-	cat("Min temp\n")
-	print(MnT)
-	cat("Precip mm\n")
-	print(Precip)
-	##  DELETE TO HERE
+	}
 	return(list(StationInf = StationInfo, MxT = MxT, MnT = MnT, Pmm = Precip, PrbPrecip = Probs))
 }
+
+
+GetPredictionTime <- function (chartExt=ForecastTableExtend, stnHead = StationInfo[2*i]){
+	now <- Sys.time()
+	PredTime <- substr(stnHead,40,47)
+	TimeDif <- now-as.POSIXlt(strptime(PredTime,format="%H%M", tz="UTC"))
+	return(now - TimeDif + chartExt$FHR*3600)  ##  This is the time that corresponds to each prediction
+}  ##  I can't figure out how to convert from UTM to EST, so this seems to be the best way to do it...
+
+
 
 
 
--- SW_sandbox/Functions_In_Progress/MakeSTImetadata.r
+++ SW_sandbox/Functions_In_Progress/MakeSTImetadata.r
@@ -2,40 +2,31 @@
 ##  Spring 2013
 ##  Contributors:  Brian Buchanan (bb386), Jo Archibald (jaa78)
 
-## Includes some pre-processing of files, and the function that will make the STI.  Examples at end..
-
-
-#library(rgdal)
-library(RSAGA)
-library(raster)
-
-rsaga.env()
-rsaga.env(workspace="G:/SaturatedAreasModeling/TownBrook/Spatial")
-envmyenv <- rsaga.env(workspace="G:/SaturatedAreasModeling/TownBrook/Spatial")
-setwd("G:/SaturatedAreasModeling/TownBrook/Spatial")
-
-##  Read in ASC file and convert it to a sgrd file:
-rsaga.esri.to.sgrd(in.grids="fill_dem_wsh.asc", out.sgrds = "DEM2", in.path="G:/SaturatedAreasModeling/TownBrook/Spatial")
-rsaga.esri.to.sgrd(in.grids="Trans2.asc", out.sgrds = "TRANS", in.path="G:/SaturatedAreasModeling/TownBrook/Spatial")
-
-
 # 	This Function will make a filtered and unfiltered STI
 #  ***Note, both TRANS and DEM grids must be in sgrd format, and in the StartFolder.  All new files will be placed in a new folder indexed with your start date and time.
 # 	Note:  User might want to play with the settings.  
 #   Dinf method used for flow accumulation
 
-
-#	SlopeOpt = "Arc", 2,5,10 (or any number)
-#	 Available Flow Directions Options:
+#	SlopeOpt = "Arc", "Tarboton", 2,5,10 (or any number)
+#	 Available Flow Directions Options [] :
 	# [0] Deterministic 8
 	# [1] Rho 8
 	# [2] Braunschweiger Reliefmodell
 	# [3] Deterministic Infinity
 	# [4] Multiple Flow Direction
 	# [5] Multiple Triangular Flow Directon
-	
-	# "+proj=utm +zone=18 ellps=WGS84"
-MakeSTI <- function(StartFolder="G:/SaturatedAreasModeling/TownBrook/Spatial", DEM = "DEM2.sgrd", TRANS = "TRANS.sgrd", DEMfrom = "USGS, 2013", TRANSfrom = "USDA, S&W shared folder", SlopeOpt = 2, FlowDirOpt = 4, user="Josephine Archibald", MapProjection = "+proj=utm +zone=18 +datum=NAD28 +units=meters" ){  # 
+
+require(RSAGA)
+require(raster)
+
+##  Read in ASC file and convert it to a sgrd file:
+#  e.g. :
+# rsaga.esri.to.sgrd(in.grids="fill_dem_wsh.asc", out.sgrds = "DEM2", in.path="G:/SaturatedAreasModeling/TownBrook/Spatial")
+# rsaga.esri.to.sgrd(in.grids="Trans2.asc", out.sgrds = "TRANS", in.path="G:/SaturatedAreasModeling/TownBrook/Spatial")
+
+
+
+MakeSTI <- function(StartFolder="G:/SaturatedAreasModeling/TownBrook/Spatial", DEM = "DEM2.sgrd", TRANS = "TRANS.sgrd", DEMfrom = "USGS, 2013", TRANSfrom = "USDA, S&W shared folder", SlopeOpt = 2, FlowDirOpt = 3, user="Josephine Archibald", MapProjection = "+proj=utm +zone=18 +datum=NAD28 +units=meters" ){  ## "+proj=utm +zone=18 ellps=WGS84"
 	
 	setwd(StartFolder) 
 	
@@ -47,23 +38,17 @@ MakeSTI <- function(StartFolder="G:/SaturatedAreasModeling/TownBrook/Spatial", D
 	# Fill DEM
 	rsaga.geoprocessor("ta_preprocessor", module=3, param=list(DEM=DEM, MINSLOPE="0.01", RESULT=paste(EndFolder,"/FILLDEM.sgrd", sep="")))  
 	
-	
 	setwd(EndFolder)
-	# Calculate Slope (using Arc option) We are using 2 options here Arc = METHOD 2, DS=2,5,10
+	# Calculate Slope (using Arc option) We are using 3 options here Arc = METHOD 2 or Tarboton ( Maximum triangular slope) = METHOD 1, DS=2,5,10
 	#  Look at options using this function: rsaga.get.usage("ta_morphometry", 0)
 	if (SlopeOpt=="Arc") {
 		rsaga.geoprocessor("ta_morphometry", module=0, param=list(ELEVATION="FILLDEM.sgrd", SLOPE="SLOPE.sgrd", ASPECT="ASPECT.sgrd",METHOD="2")) #	Calculate Slope
-		} else rsaga.geoprocessor("ta_morphometry", module=9, param=list(DEM="FILLDEM.sgrd", DISTANCE=as.character(SlopeOpt), OUTPUT="2", GRADIENT="SLOPE.sgrd"))
+		} else if (SlopeOpt=="Tarboton"){
+		rsaga.geoprocessor("ta_morphometry", module=0, param=list(ELEVATION="FILLDEM.sgrd", SLOPE="SLOPE.sgrd", ASPECT="ASPECT.sgrd",METHOD="1"))
+		}else rsaga.geoprocessor("ta_morphometry", module=9, param=list(DEM="FILLDEM.sgrd", DISTANCE=as.character(SlopeOpt), OUTPUT="2", GRADIENT="SLOPE.sgrd"))
 	
-
-	#  Calculate Catchment Area (using Dinf option)   Options:  D8(0),FD8, Rho8, MFD, D-inf, MTFD
-	    # [0] Deterministic 8
-        # [1] Rho 8
-        # [2] Braunschweiger Reliefmodell
-        # [3] Deterministic Infinity
-        # [4] Multiple Flow Direction
-        # [5] Multiple Triangular Flow Directon
-	rsaga.geoprocessor("ta_hydrology", module=0, param=list(ELEVATION="FILLDEM.sgrd", Method=3, CAREA="AREA.sgrd"))
+	#  Calculate Catchment Area   Options:  D8(0),FD8, Rho8, MFD, D-inf, MTFD
+	rsaga.geoprocessor("ta_hydrology", module=0, param=list(ELEVATION="FILLDEM.sgrd", Method=FlowDirOpt, CAREA="AREA.sgrd"))
 		
 	#  STI
 	rsaga.geoprocessor("ta_hydrology", module=20, param=list(SLOPE="SLOPE.sgrd", AREA="AREA.sgrd", CONV="1", METHOD="Standard", TRANS=paste("../", TRANS,sep=""),TWI="STI.sgrd"))
@@ -79,80 +64,22 @@ MakeSTI <- function(StartFolder="G:/SaturatedAreasModeling/TownBrook/Spatial", D
 	FDlist=c("Deterministic 8", "Rho8", "Braunschweiger Reliefmodell", "Deterministic Infinity", "Multiple Flow Direction", "Multiple Triangular Flow Directon")
 	cat(paste("Flow Direction option used was", FDlist[FlowDirOpt+1], "\n"))
 	cat(paste("Projection:", MapProjection, "\n"))
-	
 	sink()
-	
+	print(paste("flow option used was", FDlist[FlowDirOpt+1]))
+	print(paste("slope calculation option was", SlopeOpt, "(number corresponds to downslope calculation)"))
 }
 
-#
 
-##  Read in ASC file and convert it to a sgrd file:  (NOTE, puts files in wd)
-rsaga.esri.to.sgrd(in.grids="dem1.asc", out.sgrds = "DEM", in.path="E:/SaturatedAreasModeling/TownBrook/Spatial/NAD83_SAGA")
-rsaga.esri.to.sgrd(in.grids="trans_cl.asc", out.sgrds = "TRANS", in.path="E:/SaturatedAreasModeling/TownBrook/Spatial/NAD83_SAGA")
-MakeSTI(StartFolder="E:/SaturatedAreasModeling/TownBrook/Spatial/NAD83_SAGA", DEM = "DEM.sgrd", TRANS = "TRANS.sgrd", )
+print("Options for FlowDirOpt:")
+cat("		[0] Deterministic 8 
+        # [1] Rho 8 
+        # [2] Braunschweiger Reliefmodell  
+        # [3] Deterministic Infinity 
+        # [4] Multiple Flow Direction 
+        # [5] Multiple Triangular Flow Directon \n" )
 
-
-
-# setwd("G:/SaturatedAreasModeling/TownBrook/Spatial")
-# LocData=read.csv("../../../Steves/StevesProbesNAD27.csv",sep=",", header=TRUE)
- LocData=read.csv("../StevesWells83.csv",sep=",", header=TRUE)
-
-xy <- cbind(LocData$e83, LocData$n83)
-
-	
-STIgrid <- read.sgrd("STI.sgrd")
-STI_F <- read.sgrd("STI_F.sgrd")
-
-#  
-xyz = grid.to.xyz(STIgrid,varname="z")
-STIgrid <- rasterFromXYZ(xyz, res=c(NA,NA), crs="+proj=utm +zone=18 +datum=NAD27 +units=meters", digits=5)
-raster(STIgrid)
-Quants <- as.vector(quantile(xyz$z, na.rm=TRUE, probs = seq(0,1, by=0.1)))
-Lims <- as.matrix(data.frame(From=Quants[1:10], To=Quants[2:11], Becomes = seq(10,1,by=-1)))
-wet_class <- reclassify(STIgrid, Lims)  ##  depending on what version of raster, I think this function changed names...  reclass --> reclassify
-
-xyz_F = grid.to.xyz(STI_F,varname="z")
-STI_F <- rasterFromXYZ(xyz_F, res=c(NA,NA), crs="+proj=utm +zone=18 +datum=NAD27 +units=meters", digits=5)
-raster(STI_F)
-Quants_F <- as.vector(quantile(xyz_F$z, na.rm=TRUE, probs = seq(0,1, by=0.1)))
-Lims_F <- as.matrix(data.frame(From=Quants_F[1:10], To=Quants_F[2:11], Becomes = seq(10,1,by=-1)))
-wet_class_F <- reclassify(STI_F, Lims_F)  ## use to be reclass(), function changed??
-
-
-LocData["STI"] <- extract(STIgrid,xy, method='simple')
-LocData["STI_F"] <- extract(STI_F,xy, method='simple')
-LocData["Class"] <- extract(wet_class,xy, method='simple')
-LocData["Class_F"] <- extract(wet_class_F,xy, method='simple')
-summary(LocData)
-WellIDs <- read.csv("../../../StevesWells.csv") 
-
-LocData["Rank"] <- WellIDs$Rank
-
-
-# STI_F <- STIgrid
-plot(STIgrid, col=rainbow(10, end=.7), breaks=quantile(STIgrid, probs=c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1)))
-plot(STIgrid, col=rainbow(10, end=.9))
-#points(LocData$Easting, LocData$Northing)
-points(LocData$e83, LocData$n83)
-
-plot(STIgrid, col=rainbow(10, end=.7), breaks=round(quantile(STIgrid, probs=c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1)), 2), xlim=c(533500,534000), ylim=c(4688500,4689000))
-
-plot(STI_F, col=rainbow(10, end=.7), breaks=round(quantile(STI_F, probs=c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1)), 2), xlim=c(533600,534000), ylim=c(4688500,4689000))
-points(LocData$e83, LocData$n83, pch=17, col="white")
-
-plot(wet_class, col=rev(rainbow(10, start=0.2, end=.7)), xlim=c(533600,534000), ylim=c(4688500,4689000))
-plot(wet_class_F, col=rev(rainbow(10, end=.7)))
-
-head(LocData)
-
-
-##  Figuring out how to use R SAGA : 
-rsaga.get.usage("ta_morphometry", 0)
-rsaga.get.usage("ta_morphometry", 9)
-"ta_morphometry", module=0
-
-
-
+print("Options for SlopeOpt:")
+cat(	"Tarboton\nArc\nA number corresponding to distance in meters for downslope calculation\n") 
 
 
 
