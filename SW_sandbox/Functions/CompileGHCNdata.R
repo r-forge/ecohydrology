ce00d495497acda8a8a40b61beba764e310706fe
jaa78 (Josephine Archibald) <jaa78@cornell.edu>
2014-03-21 20:56:08 +0000

    CompileGHCNdata.R now is much faster by avoiding unnecessary loops, and also has an additional function that will create a continuous P,T record with optimal gages used first, and missing values filled in from a non-optimal dataset. GHCNfn_shape.r is another version of GHCNfn that has two main differences: It gets ANY gages within a boundary that has any data during the time period of interest (so doesn't have to completely span the time period), and it also allows users to grab data using a shapefile. - Jo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@205 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- SW_sandbox/Functions/CompileGHCNdata.R
+++ SW_sandbox/Functions/CompileGHCNdata.R
@@ -1,17 +1,31 @@
-CompileGHCN <- function(GHCNdata, Start = "2000-01-01", End = "2013-12-31", wgt=rep(1,length(GHCNdata))) {  
+Comp2GHCN#  Function created 3/21/2014 by Jo Archibald (jaa78@cornell.edu)
+
+##  TWO functions here:  
+## CompileGHCN :  reads in output from getGHCN_shape or getGHCN and combines the P, Tn and Tx from all stations to give an average P (mm), Tn (mm) and Tx (mm).
+## Comp2GHCN : Calls the CompileGHCN function for 2 separate lists of GHCN datasets.
+#  It defaults to the vales from the first object, only uses the second to fill in missing values.  It returns a column called P_filled = TRUE when P had to be filled from the second GHCN object.
+
+
+CompileGHCN <- function(GHCNdata, Start = "2000-01-01", End = "2013-12-31", wgt=rep(1,length(GHCNdata)), silent=FALSE) {  
   DATE <- seq(as.Date(Start), as.Date(End), by=1)
+  GHCNdata[[length(GHCNdata)]] <- NULL  #  Remove the last element of GHCNdata, StationInfo
   
   #  Create a data frame containing all the precip and temp data in the GHCN record, 
   #   and converting units to mm and C
   NOAA_gages <- as.data.frame(matrix(nrow=length(DATE), ncol=1+3*length(GHCNdata)))
-  names(NOAA_gages) <- c("DATE", paste("P", seq(1,length(GHCNdata)), sep=""), paste("Tn", seq(1,length(GHCNdata)), sep=""), paste("Tx", seq(1,length(GHCNdata)), sep=""))
+  names(NOAA_gages) <- c("DATE", paste("P", seq(1,length(GHCNdata)), sep=""), paste("Tx", seq(1,length(GHCNdata)), sep=""), paste("Tn", seq(1,length(GHCNdata)), sep=""))
   NOAA_gages$DATE <- DATE
-  for (i in 1:length(DATE)){
-    for (j in 1:length(GHCNdata)){
-      NOAA_gages[,(j+1)][i] <- if(length(which(GHCNdata[[j]]$date==DATE[i]))==1 & length(GHCNdata[[j]]$PRCP)>0) GHCNdata[[j]]$PRCP[which(GHCNdata[[j]]$date==DATE[i])]/10 else NA
-      NOAA_gages[,(j+1+length(GHCNdata))][i] <- if(length(which(GHCNdata[[j]]$date==DATE[i]))==1 & length(GHCNdata[[j]]$TMIN)>0) GHCNdata[[j]]$TMIN[which(GHCNdata[[j]]$date==DATE[i])]/10 else NA
-      NOAA_gages[,(j+1+2*length(GHCNdata))][i] <- if(length(which(GHCNdata[[j]]$date==DATE[i]))==1 & length(GHCNdata[[j]]$TMAX)>0) GHCNdata[[j]]$TMAX[which(GHCNdata[[j]]$date==DATE[i])]/10 else NA
-    }
+  
+  for (j in 1:length(GHCNdata)){ 
+      dat <- GHCNdata[[j]][which(GHCNdata[[j]]$date>=DATE[1] & GHCNdata[[j]]$date<=max(DATE)),]
+	  
+	  notMissing <- data.frame(dat, as.data.frame(matrix(nrow=nrow(dat), ncol=(4-ncol(dat)))))   ## This assumes that we never have Tn or Tx without PRCP first..  Otherwise the Tn and Tx will be put into precip column..
+	  Missing <- data.frame(DATE = DATE[which(!DATE %in% dat$date)], as.data.frame(matrix(nrow=length(which(!DATE %in% dat$date)), ncol=3)))
+	  names(Missing) = names(notMissing)  ## assign the same names to the Missing data frame so that we can use rbind()
+	  New <- rbind(Missing, notMissing)
+	  NOAA_gages[,c((j+1),(j+1+length(GHCNdata)),(j+1+2*length(GHCNdata)))] <- 
+	  New[order(New$date),2:4] / 10  ##  CHANGE HERE if the getGHCN function changes
+
   }
   
   #  Replace -999.9 with a value of NA
@@ -21,20 +35,43 @@ CompileGHCN <- function(GHCNdata, Start = "2000-01-01", End = "2013-12-31", wgt=
   }
   NOAA_gages[,2:ncol(NOAA_gages)]<-sapply(NOAA_gages[,2:ncol(NOAA_gages)], ReplaceNA)
   
-  ##  Allows us to weight the different gages (must be non-negative integers. Zero values removes that data-set)
-  wtmean <- function(x,y){
-    return(sum(na.omit(x*y))/sum(na.omit((x+y-x))))
-  }
-  
-  if (mean(wgt)==1 & sd(wgt)==0){
-    NOAA_gages$P <- sapply(apply(NOAA_gages[,2:(1+length(GHCNdata))],MARGIN=1,FUN=na.omit),MARGIN=1,FUN=mean)
-    NOAA_gages$Tn <- sapply(apply(NOAA_gages[,(2+length(GHCNdata)):(1+2*length(GHCNdata))],MARGIN=1,FUN=na.omit),MARGIN=1,FUN=mean)
-    NOAA_gages$Tx <- sapply(apply(NOAA_gages[,(2+2*length(GHCNdata)):(1+3*length(GHCNdata))],MARGIN=1,FUN=na.omit),MARGIN=1,FUN=mean)
-  } else{  ##  If we want to weight the gages differently, it takes much longer
-    NOAA_gages$P <- apply(NOAA_gages[,2:(1+length(GHCNdata))],MARGIN=1,FUN=wtmean, y=wgt)
-    NOAA_gages$Tn <- apply(NOAA_gages[,(2+length(GHCNdata)):(1+2*length(GHCNdata))],MARGIN=1,FUN=wtmean, y=wgt)
-    NOAA_gages$Tx <- apply(NOAA_gages[,(2+2*length(GHCNdata)):(1+3*length(GHCNdata))],MARGIN=1,FUN=wtmean, y=wgt)
-  }
-  print(summary(NOAA_gages))
+	NOAA_gages$P <- sapply(apply(NOAA_gages[,2:(1+length(GHCNdata))],MARGIN=1,FUN=na.omit),MARGIN=1,FUN=mean)
+	if (length(sapply(apply(NOAA_gages[,(2+length(GHCNdata)):(1+2*length(GHCNdata))],MARGIN=1,FUN=na.omit),MARGIN=1,FUN=mean)) == nrow(NOAA_gages)){  #  If no temp data is available, there will be no Tn or Tx column returned
+	NOAA_gages$Tx <- sapply(apply(NOAA_gages[,(2+length(GHCNdata)):(1+2*length(GHCNdata))],MARGIN=1,FUN=na.omit),MARGIN=1,FUN=mean)
+	NOAA_gages$Tn <- sapply(apply(NOAA_gages[,(2+2*length(GHCNdata)):(1+3*length(GHCNdata))],MARGIN=1,FUN=na.omit),MARGIN=1,FUN=mean)
+	}
+	
+  if (!silent) print(summary(NOAA_gages))
   return(NOAA_gages)
 }
+
+
+##  This function tries to create a continuous daily record of P and max/min temps for GHCN gage information from output from the getGHCN and/or getGHCN_shape functions
+#  Arguments: GHCNopt:  These are the optimal GHCN stations, for example, gages within the watershed.
+#  GHCN2:  GHCN gages that can be used to fill in the missing values from the GHCNopt records
+
+Comp2GHCN <- function(GHCNopt, GHCN2, Start = "2000-01-01", End = "2013-12-31"){
+	g1 <- CompileGHCN(GHCNopt, Start = "2000-01-01", End = "2013-12-31", silent=TRUE)
+	g2 <- CompileGHCN(GHCN2, Start = "2000-01-01", End = "2013-12-31", silent=TRUE)
+	
+	if ("P" %in% names(g1)){
+	P <- g1$P
+	P_filled <- rep(FALSE, length(P))
+	P_filled[which(is.na(P))] <- TRUE
+	P[which(is.na(P))] <- g2$P[which(is.na(P))]
+	} else P <- g2$P
+	if ("Tn" %in% names(g1)){
+	Tn <- g1$Tn
+	Tn[which(is.na(Tn))] <- g2$Tn[which(is.na(Tn))]
+	} else Tn <- g2$Tn
+	if ("Tx" %in% names(g1)){
+	Tx <- g1$Tx
+	Tx[which(is.na(Tx))] <- g2$Tx[which(is.na(Tx))]
+	} else Tx <- g2$Tx
+	return(data.frame(DATE=g1$DATE, P, Tn, Tx, P_filled))	
+}
+
+
+
+
+
---
+++ SW_sandbox/Functions/GHCNfn_shape.r
@@ -0,0 +1,128 @@
+##  Grab GHCN data centered on a particular location.
+
+##  Arguments: 
+#	lat (degrees Lat)
+#	long (degrees Lon)
+#	NSdeg (distance (in deg) N-S for search box)
+#	EWdeg (distance (in deg) E-W for search box)
+#	StartYear
+#	EndYear
+#	StnOpt:  Default, "Any" will return all stations within the search window that have at least a P or T dataset.  
+#		"Full" returns only stations that have all three values
+# 	Note:  Try to work inside a working directory that already contains the files "ghcndailytmp.dly" and "ghcnd-stations.txt"
+	#	created previously so you don't need to re-download the inventory every time you run the function
+#		inside your current working directory to run this function.
+##  Returns :  List object with data frames containing date, PRCP (tenths of mm), TMAX (tenths of C), TMIN (tenths of C), 
+					#	and Lat/Lon (decimal degrees) and Elev (m)
+					
+#  SPATIAL INFORMATION:
+#	useShape:  defaults to FALSE - in that case uses the search box defined by NSdeg, EWdeg
+# boundaryFold = character:  the folder containing the shapefile 
+# boundary = character: shapefile name w/o extension (i.e. don't include ".shp") 
+# projection =  proj4string of input shapefile (needed if not defined in the layer)
+# TransformLatLon = default FALSE - change to true if the shapefile needs to be projected into lat/lon
+# LLproj : the proj4string for the lat/lon coordinates, not needed if the shapefile doesn't need to be transformed (default is for the NE USA?)
+					
+					
+
+getGHCN_shape <- function(lat=NULL, long=NULL, NSdeg=0.5, EWdeg=0.5, StartYear= 2000, EndYear=(as.POSIXlt(Sys.time())$year+1900), StnOpt = "Any", useShape=FALSE, boundaryFold=getwd(), boundary=NULL, projection=NULL, TransformLatLon = FALSE, LLproj = "+proj=longlat +ellps=WGS84 +datum=WGS84" ){
+	
+	require(SWATmodel)
+	require(GhcnDaily)
+	require(plyr)
+	require(rgdal)
+	require(maptools)
+	
+	read_ghcn_raw=function(GHCNfilename,startyear=StartYear,elements=c("TMIN","TMAX","PRCP")){
+		 require(Hmisc)
+		 suppressWarnings(rm(list=c("alldata")))
+		 suppressWarnings(rm(list=objects(pattern="allghcndata")))
+		 for (fileline in readLines(GHCNfilename)){
+		  stn=substr(fileline,1,11)
+		  year=substr(fileline,12,15)
+		  mo=substr(fileline,16,17)
+		  yearmo=paste(year,mo,sep="")
+		  element=as.character(substr(fileline,18,21))
+		  if(year<startyear | (length(grep(element,elements))<1)){next()}
+		  dy=1:monthDays(paste(year,mo,"01",sep="/"))
+		  date=as.Date(format="%Y%m/%d",paste(yearmo,"/",dy,sep=""))
+		  tempdata=data.frame(date,var=as.numeric(as.character(substring(fileline,c(seq(22,((length(dy)-1)*8+22),8)),c(seq(26,((length(dy)-1)*8+26),8))))))
+		  names(tempdata)=c("date",element)
+		  if(!(exists(paste("allghcndata",element,sep="")))){
+			assign(paste("allghcndata",element,sep=""),tempdata)
+		  } else {
+			assign(paste("allghcndata",element,sep=""),rbind(get(paste("allghcndata",element,sep="")),tempdata))
+		  }
+		 }
+		 for (dfnames in objects(pattern="allghcndata")){
+		   print(dfnames)
+		   if(exists("alldata")){alldata=merge(alldata,get(dfnames),by="date",all=T)}
+		else {alldata=get(dfnames)}
+		 }
+		 return(alldata)
+	}
+	
+	
+	if (!("ghcndailytmp.dly" %in% list.files()) ) downloadDailyInventory()  # only download if needed
+	junk=readDailyInventory()
+
+	##  OPTIONAL: USING A SHAPEFILE ----------------------------------------------------
+	if (useShape) { ##  
+		if (is.null(projection)){	
+			SHP <- readOGR("GIS", "CayIn")
+		} else SHP <- readShapePoly(paste(boundaryFold, boundary, sep="/"), proj4string=CRS(projection)) 
+		if (TransformLatLon) SHP <- spTransform(SHP, CRS(LLproj))  #  Transform the shapefile into lat/lon if it isn't already
+		ALLgages <- junk
+		coordinates(ALLgages) <- ALLgages[,3:2]
+		attributes(ALLgages)$proj4string <- attributes(SHP)$proj4string 
+		ALLgages$wshd <- over(ALLgages, SHP)$Id   # This will be NA for gages outside basin
+		Sub1 <- ALLgages[-which(is.na(ALLgages$wshd)),]  #  Just grab gages within watershed
+		SubJunk <- subset(Sub1, LastYear> StartYear  & FirstYear < EndYear & 
+		(Element =="TMAX" | Element=="TMIN" | Element=="PRCP"))  # Make sure it has data within our time of interest and have P or T
+	# ---------------------------------------------------------------------------
+	} else 	SubJunk <- subset(junk, 
+	Lat > (lat-NSdeg) & Lat < (lat+NSdeg) & 
+	Lon > (long-EWdeg) & Lon < (long+EWdeg)& 
+	LastYear> StartYear  & FirstYear < EndYear & 
+	(Element =="TMAX" | Element=="TMIN" | Element=="PRCP"))
+	
+	if (StnOpt == "Full"){   #  Only returns stations that have max/min temp and precip
+	GHCNstns <- subset(count(SubJunk[1]) ,freq==3)
+	} else GHCNstns <- SubJunk  #  Otherwise returns all stations with any information
+	
+	if (nrow(GHCNstns) < 1){ 
+		print("no GHCN stations within your search window, you could try making NSdeg or EWdeg larger to expand it")
+	} else {
+	
+	GHCN <-list()
+
+	if (!("ghcnd-stations.txt" %in% list.files()) ) download.file(GHCN.DAILY.METADATA.URL, destfile="ghcnd-stations.txt")
+	ghcnSta = readLines("ghcnd-stations.txt") 
+		
+	for (b in GHCNstns$Id){
+		download.file(paste("ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/all/", b, ".dly", sep=""), "ghcndailytmp.dly")
+		GHCN[[b]] <- read_ghcn_raw("ghcndailytmp.dly")
+	}	
+	Stns <- names(GHCN)    
+	Lats <- SubJunk$Lat[sapply(Stns, function(x) min(grep(x, SubJunk$Id)))]
+	Lons <- SubJunk$Lon[sapply(Stns, function(x) min(grep(x, SubJunk$Id)))]
+	subst =   ghcnSta[sapply(Stns, function(n) grep(n, ghcnSta))] 
+	ghcnElev = 	as.numeric(sapply(strsplit(subst, " +"), function(s) s[4]))
+	sy <- SubJunk$FirstYear[sapply(Stns, function(x) max(grep(x, SubJunk$Id)))]  ##  Grabbing first year of PRCP (always listed after TMAX and TMIN)
+	ly <- SubJunk$LastYear[sapply(Stns, function(x) max(grep(x, SubJunk$Id)))]  ##  Grabbing last year of PRCP 
+	pnas <- as.vector(sapply(GHCN, function(x) length(which(x$PRCP== -9999))/nrow(x)))
+	StationInfo = data.frame(ID=Stns, Lat=Lats, Lon=Lons, Elev=ghcnElev,Start_PRCP=sy, End_PRCP=ly, PercNAs_PRCP=pnas)
+	
+	#GHCN[[b]]$Lat <- SubJunk$Lat[min(which(SubJunk$Id == b))]    ##  Add a column reporting decimal latitude
+		#GHCN[[b]]$Lon <- SubJunk$Lon[min(which(SubJunk$Id == b))]	 ##  Add a column reporting decimal longitude
+
+		#GHCN[[b]]$Elev <- as.numeric(ghcnElev)          ##  Add a column reporting decimal longitude
+
+	GHCN[['StationInfo']] = StationInfo
+	return(GHCN)	
+	}
+
+}
+
+
+
