de3e9c6f72467ba343898a734e7340b5b3413842
jaa78 (Josephine Archibald) <jaa78@cornell.edu>
2014-08-12 20:22:12 +0000

    Removed an unused conditional statement.  -Jo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@212 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydRology/R/Solar.R
+++ pkg/EcoHydRology/R/Solar.R
@@ -1,7 +1,6 @@
 Solar <-  function (lat, Jday, Tx, Tn, albedo=0.2, forest=0, slope=0, aspect = 0, units="kJm2d", latUnits = "unknown", printWarn=TRUE) {
 	
 	if ((abs(lat) > pi/2 & latUnits == "unknown") | latUnits == "degrees" ){
-		if (printWarn==TRUE) 
 		lat <- lat*pi/180
 	} else if (latUnits == "unknown"){
 		if (printWarn==TRUE) warning("In Solar(): Input latitude units are not specified and assumed to be radians")
