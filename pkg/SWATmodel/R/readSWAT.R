7999d3b0d215db5cd31e88550bd70cb3cf7c5dd7
drfuka (Mr. ) <drf28@cornell.edu>
2015-02-13 22:03:51 +0000

    New readSWAT.R Function update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@215 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/SWATmodel/R/readSWAT.R
+++ pkg/SWATmodel/R/readSWAT.R
@@ -1,17 +1,41 @@
 readSWAT <-
-function(outfile_type){
+function(outfile_type="rch",pathtofile="./"){
 if(missing(outfile_type)){print(" 'outfile_type' is missing, should be rch, sub, or.. ask drf28 for other types")}
+sfilename=file(paste(pathtofile,"output.",outfile_type,sep=""))
+start_year = read.fortran(textConnection(readLines(paste(pathtofile,"file.cio",sep=""))[9]), "f20")
+test = readLines(sfilename)
+unlink(sfilename)
+test=test[9:length(test)]
+linelength=nchar(test[2])
 if (outfile_type=="sub"){
-   varformat="x6,a4,1x,a8,1x,a4,a10,30a10"
-   dataformat="x6,i4,1x,i8,1x,i4,f10,30f10"
+   datalength=10
+   datastartlocs=c(1,11,20,seq(25,linelength-1,datalength))
+   datastoplocs=c(10,19,24,seq(24+datalength,linelength,datalength))
+   numvars=(linelength-25)/datalength
+#
+#  1000 format ('BIGSUB',i4,1x,i8,1x,i4,e10.5,18e10.3,1x,e10.5,3e10.3)
+#
+   if(length(datastartlocs) >= 21){
+    datastartlocs[21:length(datastartlocs)]=datastartlocs[21:length(datastartlocs)]+1
+    datastoplocs[21:length(datastoplocs)]=datastoplocs[21:length(datastoplocs)]+1
+   }
 } else if (outfile_type=="rch"){
-   varformat="x6,a4,1x,a8,1x,a5,30a12"
-   dataformat="x6,i4,1x,i8,1x,i5,30f12"
+   datalength=12
+   datastartlocs=c(1,11,20,seq(26,linelength,datalength))
+   datastoplocs=c(10,19,25,seq(25+datalength,linelength,datalength))
+   numvars=(linelength-25)/datalength
 } else { print ("You need to add your file type to this function if it is not output.sub or output.rch")}
-  vfrformat = unlist(strsplit(as.character(varformat), ","))
-  dfrformat = unlist(strsplit(as.character(dataformat), ","))
-  outvars=read.fortran(paste("output.",outfile_type,sep=""),vfrformat,skip=8,nrows=1)
-  outdata=read.fortran(paste("output.",outfile_type,sep=""),dfrformat,skip=9,col.names=outvars)
-  return(outdata)
+  test[1]=gsub("#","X",test[1])
+  test[1]=gsub("/","Y",test[1])
+  swatcolnames=gsub(" ","",substring(test[1],datastartlocs,datastoplocs))
+  swatcolnames=gsub("X","_no_",swatcolnames)
+  swatcolnames=gsub("Y","_per_",swatcolnames)
+  test=test[2:length(test)]
+  swatfiledata=data.frame(lapply(1:length(swatcolnames),function(x){return(data.frame(substr(test,datastartlocs[x],datastoplocs[x])))}))
+  colnames(swatfiledata)<-swatcolnames
+  swatfiledata[, c(1:numvars+3)] <- sapply(swatfiledata[, c(1:numvars+3)], as.character)
+  swatfiledata[, c(2:numvars+3)] <- sapply(swatfiledata[, c(2:numvars+3)], as.numeric)
+  swatfiledata$mdate = as.Date(floor((row(swatfiledata)[,1]-1)/length(unique(swatfiledata$RCH)))+1, origin = paste(start_year - 1, "-12-31", sep = ""))
+  return(swatfiledata)
 }
 
