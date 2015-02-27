fa1f10bf5d9e3c5322ae9f1af651a677c559e070
drfuka (Mr. ) <drf28@cornell.edu>
2015-02-27 03:04:20 +0000

    Forcing some fixes into change_params
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@218 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydRology/R/calib_swat_ex.R
+++ pkg/EcoHydRology/R/calib_swat_ex.R
@@ -6,6 +6,10 @@ change_params=""
 rm(change_params)
 load(paste(path.package("EcoHydRology"), "data/change_params.rda", sep = "/"))
 #data(change_params)
+change_params[change_params$parameter=="SMFMX",]$min=0
+change_params[change_params$parameter=="SMFMN",]$min=0
+change_params[change_params$parameter=="ALPHA_BF",]$max=1.0
+change_params[change_params$parameter=="TIMP",]$max=1.0
 calib_range=c("1999-12-31","2010-12-31")
 params_select=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,19,21,23,24,32,33)
 calib_params=change_params[params_select,]
