912556b7ad2c2382beceda7b31c9efd2461404d6
da373 (Dan Auerbach) <auerbach.dan@gmail.com>
2015-04-27 16:16:41 +0000

    dan A breaking flood
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@225 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TauDEM/R/flood.R
+++ pkg/TauDEM/R/flood.R
@@ -1,42 +1,27 @@
-flood <- function(DEM,res,degree) {
+#the value expected by the .C call that was previously called "nrow" is actually the x-dim/ncols of either SpatialGrid or raster objects
+#changed to "x-dim/y-dim" with same .C call ordering...
 
-  ## data preparation
-
-  stopifnot(is(DEM,"matrix"))
-
-  if(min(as.vector(DEM[!is.na(DEM)])) < -9000)
-     stop("DEM contains unrealistic values (< -9000)")
-
-  DEM[is.na(DEM)] <- -9999
-  
-  nrow <- dim(DEM)[1]
-  ncol <- dim(DEM)[2]
-
-  ##  exclude an area of the DEM from the calculations
-  ##  still to implement in R (using NA's)
-
-  result <- .C("flood",
-               PACKAGE = "TauDEM",
-               as.double(DEM),
-               result = double(nrow*ncol + 2),
-               as.integer(nrow),
-               as.integer(ncol),
-               as.double(res),
-               as.double(degree))$result
-
-  ## formatting of the results
-  ## the first position in result gives the number of iterations that have been performed
-
-  result[result > 999998] <- NA
-
-  if(result[1] == -1) cat("incomplete sink removal\n")
-  else {
-    if(result[1] == 100) cat("Maximum number of iterations reached (100).\nSink removal is probably not complete; please run again\n")
-    else cat("number of iterations = ", result[1], "\n")
+flood = function (DEM, degree=1)
+{
+  stopifnot( class(DEM) %in% c("SpatialGridDataFrame", "RasterLayer") )
+  if (class(DEM)=="SpatialGridDataFrame") {
+    senddata=DEM@data$band1
+    xdim = DEM@grid@cells.dim[1]
+    ydim = DEM@grid@cells.dim[2]
+    res=DEM@grid@cellsize[1]
+  } else {
+    senddata=values(DEM)
+    xdim = DEM@ncols
+    ydim = DEM@nrows
+    res=res(DEM)[1] #will fail for a raster w/different x&y resolution...probably a good thing      
   }
-
-  cat("number of sinks left = ", result[2], "\n")
-
-  return(matrix(result[3:(nrow*ncol+2)], nrow = nrow))
-
+  senddata[is.na(senddata)]=-9999
+  result <- .C("flood", PACKAGE = "TauDEM", as.double(senddata),
+               result = double(xdim * ydim), as.integer(xdim), as.integer(ydim),
+               as.double(res), as.double(degree))$result
+  result[result < 0] <- NA
+  if (class(DEM)=="SpatialGridDataFrame") {DEM@data$band1=result} else {values(DEM)=result}
+  return(DEM)
 }
+
+
