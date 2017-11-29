1974c2b87beec8d0c2e999c1a9efe3e298d79191
drfuka (Mr. ) <drf28@cornell.edu>
2017-11-29 16:15:41 +0000

    updated http to https
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@241 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydRology/R/get_usgs_gage.R
+++ pkg/EcoHydRology/R/get_usgs_gage.R
@@ -3,7 +3,7 @@ get_usgs_gage<-function(flowgage_id,begin_date="1979-01-01",end_date="2013-01-01
 # Grabs USGS stream flow data for 1979 to present... to align with CFSR datasets.
 #
 
-url = paste("http://waterdata.usgs.gov/nwis/inventory?search_site_no=", flowgage_id, "&search_site_no_match_type=exact&sort_key=site_no&group_key=NONE&format=sitefile_output&sitefile_output_format=rdb&column_name=station_nm&column_name=site_tp_cd&column_name=dec_lat_va&column_name=dec_long_va&column_name=alt_va&column_name=drain_area_va&column_name=contrib_drain_area_va&column_name=rt_bol&list_of_search_criteria=search_site_no",sep="")
+url = paste("https://waterdata.usgs.gov/nwis/inventory?search_site_no=", flowgage_id, "&search_site_no_match_type=exact&sort_key=site_no&group_key=NONE&format=sitefile_output&sitefile_output_format=rdb&column_name=station_nm&column_name=site_tp_cd&column_name=dec_lat_va&column_name=dec_long_va&column_name=alt_va&column_name=drain_area_va&column_name=contrib_drain_area_va&column_name=rt_bol&list_of_search_criteria=search_site_no",sep="")
 
 gage_tsv=readLines(url)
 gage_tsv=gage_tsv[grep("^#",gage_tsv,invert=T)][c(1,3)]
@@ -19,7 +19,7 @@ gagename = tempdf$station_nm
 begin_date = as.character(begin_date)
 end_date = as.character(end_date)
 
-url = paste("http://nwis.waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&begin_date=",begin_date,"&end_date=",end_date,"&site_no=", flowgage_id, sep = "")
+url = paste("https://nwis.waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&begin_date=",begin_date,"&end_date=",end_date,"&site_no=", flowgage_id, sep = "")
 flowdata_tsv=gage_tsv=readLines(url)
 flowdata_tsv=flowdata_tsv[grep("^#",flowdata_tsv,invert=T)][c(3:length(flowdata_tsv))]
 flowdata = read.delim(text=flowdata_tsv,header=F,sep="\t",col.names = c("agency", "site_no", "date", "flow", "quality"), colClasses = c("character", "numeric", "character", "character", "character"), fill = T)
