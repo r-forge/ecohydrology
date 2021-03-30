e0aa104d06af544ee59b330191240fef8e893587
drfuka <drfuka@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-03-30 00:59:39 +0000

    moving warmup period out of the objective function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@263 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydRology/R/swat_objective_function_rch.R
+++ pkg/EcoHydRology/R/swat_objective_function_rch.R
@@ -18,9 +18,7 @@ swat_objective_function_rch=function (x, calib_range, calib_params, flowgage, rc
   test2 = subset(outdata, outdata$RCH == rch)
   test3 = merge(flowgage$flowdata, test2, all = F)
   test3$Qm3ps = test3$flow/3600/24
-  NS = NSeff(test3$Qm3ps[730:(length(test3$Qm3ps))], test3$FLOW_OUTcms[730:(length(test3$Qm3ps))])
-  print(NS)
-  if(save_results){file.copy(list.files(),"../")}
+  NS = NSeff(test3$Qm3ps, test3$FLOW_OUTcms,"../")}
   file.remove(list.files())
   setwd("../")
   file.remove(tmpdir)
