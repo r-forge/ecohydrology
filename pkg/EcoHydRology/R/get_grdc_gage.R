c5d82604dae25ce2a826a3e1a7d2a9c1825a99e4
drfuka (Mr. ) <drf28@cornell.edu>
2022-02-14 01:42:16 +0000

    gaugetmp issue debug
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@288 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydRology/R/get_grdc_gage.R
+++ pkg/EcoHydRology/R/get_grdc_gage.R
@@ -40,8 +40,8 @@ get_grdc_gage=function(filename=grdcfilename){
   nskipline = grep("Next downstream station:", readLines(filename))[1]-1
   GRDC_Next_downstream_station=as.numeric(strsplit(read_lines(filename,n_max = 1,skip=nskipline),":")[[1]][2])
 
-  print(head(gaugetab))
   gaugetab=gaugetab[!(gaugetab$Value %in% c(-999, -99, -9999, 99, 999, 9999))]
+  print(head(gaugetab))
 
   GRDC_mindate=min(gaugetab$dates)
   GRDC_maxdate=max(gaugetab$dates)
