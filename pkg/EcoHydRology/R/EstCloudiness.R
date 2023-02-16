e6c6f7e8e6aef683d7cf28248676c3c0c46ebe97
drfuka (Mr. ) <drf28@cornell.edu>
2023-02-16 05:00:23 +0000

    fixing EstCloud
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecohydrology@304 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EcoHydRology/R/EstCloudiness.R
+++ pkg/EcoHydRology/R/EstCloudiness.R
@@ -1,8 +1,5 @@
-EstCloudiness <- function (Tx=(-999), Tn=(-999), trans=NULL, transMin = 0.15, transMax = 0.75, opt = "linear") 
+EstCloudiness <- function (Tx, Tn, trans=NULL, transMin = 0.15, transMax = 0.75, opt = "linear") 
 {
-    suppressWarnings(if ((Tx == -999 | Tn == -999) & is.null(trans)){ 
-		print("Error: Please enter either Max&Min temp or transmissivity")
-		} else {
 	if (is.null(trans))	trans <- transmissivity(Tx, Tn)
     if (opt=="Black") {
 		cl <- (0.34 - sqrt(0.34^2 + 4*0.458*(0.803-trans)))/(-2*0.458)
@@ -13,6 +10,4 @@ EstCloudiness <- function (Tx=(-999), Tn=(-999), trans=NULL, transMin = 0.15, tr
 	cl[which(cl > 1)] <- 1
     cl[which(cl < 0)] <- 0
     return(cl)
-	} )
 }
-
